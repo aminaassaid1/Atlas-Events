0b22f217fff17eca177c9e601f0eea5f6ab293e8
Amina Assaid <aminaassaid123@gmail.com>
2026-01-14 16:21:01 +0100

    Initial commit

--- Atlas-For-Events-main/src/components/Header.tsx
+++ Atlas-For-Events-main/src/components/Header.tsx
@@ -81,7 +81,7 @@ const Header = () => {
             {/* Mobile Menu Button */}
             <button
               onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-              className="xmenu-toggler lg:hidden float-right mt-4.5 mb-4 md:ml-7 ml-4 size-11 bg-dark-600 relative cursor-pointer max-lg:order-1"
+              className="xmenu-toggler float-right mt-4.5 mb-4 md:ml-7 ml-4 size-11 bg-dark-600 relative cursor-pointer max-lg:order-1"
               type="button"
             >
               <span className={`block absolute left-2.5 h-0.5 rounded-px bg-white duration-300 top-3.25 w-5.5 ${isMobileMenuOpen ? 'rotate-45 top-5.5' : ''}`}></span>
